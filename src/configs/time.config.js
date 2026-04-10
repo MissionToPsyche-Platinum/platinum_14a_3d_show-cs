93c82f2b172665f4d102455827ada0797f7863b7
Braeden West <braedenwest1@gmail.com>
2026-04-09 17:36:00 -0700

    Time overlay fixes

--- src/configs/time.config.js
+++ src/configs/time.config.js
@@ -2,27 +2,62 @@ export const timeTimeLine = [
     {
         visibility: {
             startVH: 1,
-            endVH: 5,
-            fadeInDuration: 0.5,
-            fadeOutDuration: 0.5,
+            endVH: 8.25,
+            fadeInDuration: 0.1,
+            fadeOutDuration: 0,
         },
         timeBounds: {
             // only month is 0 indexed
-            startTime: new Date(2023, 0, 1),
-            endTime: new Date(2024, 0, 1)
+            startTime: new Date(2023, 9, 13),
+            endTime: new Date(2023, 9, 13)
         }
     },
     {
         visibility: {
-            startVH: 7,
-            endVH: 10,
-            fadeInDuration: 0.5,
-            fadeOutDuration: 0.5,
+            startVH: 8.25,
+            endVH: 9.75,
+            fadeInDuration: 0,
+            fadeOutDuration: 0,
         },
         timeBounds: {
-            // only month is 0 indexed
-            startTime: new Date(2024, 6, 15),
-            endTime: new Date(2025, 0, 1)
+            startTime: new Date(2023, 9, 13),
+            endTime: new Date(2026, 4, 1)
+        }
+    },
+    {
+        visibility: {
+            startVH: 9.75,
+            endVH: 14.25,
+            fadeInDuration: 0,
+            fadeOutDuration: 0,
+        },
+        timeBounds: {
+            startTime: new Date(2026, 4, 1),
+            endTime: new Date(2026, 4, 1)
+        }
+    },
+    {
+        visibility: {
+            startVH: 14.25,
+            endVH: 15.75,
+            fadeInDuration: 0,
+            fadeOutDuration: 0,
+        },
+        timeBounds: {
+            startTime: new Date(2026, 4, 1),
+            endTime: new Date(2029, 7, 1)
+        }
+    },
+    {
+        visibility: {
+            startVH: 15.75,
+            endVH: 20,
+            fadeInDuration: 0,
+            fadeOutDuration: 0,
+        },
+        timeBounds: {
+            startTime: new Date(2029, 7, 1),
+            endTime: new Date(2029, 7, 1)
         }
     }
 ]
