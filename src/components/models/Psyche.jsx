5dcd1719fa263e0920824f10ed02c25ecc6c19ae
Braeden West <braedenwest1@gmail.com>
2026-04-22 03:10:38 -0700

    Webpage title and logo

--- src/components/models/Psyche.jsx
+++ src/components/models/Psyche.jsx
@@ -2,7 +2,7 @@ import { useGLTF } from '@react-three/drei'
 import { useMemo } from 'react'
 
 export default function Psyche({position = [0, 0, 0], scale = 1, rotation = [0, 1.5, -0.6]}) {
-    const gltf = useGLTF(`${import.meta.env.BASE_URL}models/psyche.glb`)
+    const gltf = useGLTF(`${import.meta.env.BASE_URL}models/Psyche.glb`)
     const scene = useMemo(() => gltf.scene.clone(true), [gltf.scene])
 
     return (
